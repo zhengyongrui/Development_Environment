fb3a845be98dc561b12a9185978883a10a98cd73
zyr <zyr@outlook.com>
2024-04-02 21:36:42 +0800

    nodejs配置镜像仓库命令

---
+++ docker/data/nodejs/changRegistry.bat
@@ -0,0 +1,4 @@
+@REM 安装npm后需要通过此命令修改仓库地址
+npm config set registry https://registry.npm.taobao.org --global
+@REM 安装node-sass后需要通过此命令修改仓库地址，这样安装更加简单
+npm config set sass_binary_site=https://npm.taobao.org/mirrors/node-sass
